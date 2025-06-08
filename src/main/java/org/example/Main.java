ff2c7482ef6c7b03c6d7e35c9d2edbb11d3da6a5
IngaSam <Kayumovainga2@mail.ru>
2025-06-08 17:34:03 +0300

    first creat bean

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext("org.example");
+        Task task = context.getBean(Task.class); //вывод по классу
+        Task task2 = (Task) context.getBean("main-task"); // вывод по его имени
+        System.out.println(task);
+        System.out.println(task2);
+        System.out.println(task==task2);
+
+        TaskManager taskManager = (TaskManager)context.getBean("taskManager");
+        taskManager.printTask();
+       }
+}
---
+++ src/main/java/org/example/TaskManager.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TaskManager {
+    private final Task task;
+    @Autowired
+    public TaskManager(Task task) {
+        this.task = task;
+    }
+    public void printTask(){
+        System.out.println("Current task: " + task.toString());
+    }
+}
