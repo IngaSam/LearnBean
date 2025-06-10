08411418523d9be95165622065c99fa973430720
IngaSam <Kayumovainga2@mail.ru>
2025-06-10 13:27:02 +0300

    add PreDestroy and PostConstruct in TaskManager

--- src/main/java/org/example/TaskManager.java
+++ src/main/java/org/example/TaskManager.java
@@ -1,14 +1,27 @@
 package org.example;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+//@Scope("prototype")
 public class TaskManager {
     private final Task task;
 
     public TaskManager(Task task) {
       this.task = task;
+        System.out.println("call taskManager constructor");
+    }
+    @PostConstruct
+    public void init() {
+        System.out.println("call taskManager init");
+    }
+    @PreDestroy
+    public void destroy() {
+        System.out.println("call taskManager destroy");
     }
 
     public void printTask(){
